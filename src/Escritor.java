1627fb043d6e8a867bd86c5fe3419898dbb9f4ab
Alex-Laptop <alex03marcos@gmail.com>
2023-03-06 10:23:49 +0100

    metodo escribe

--- src/Escritor.java
+++ src/Escritor.java
@@ -1,13 +1,19 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
 
 public class Escritor {
+    private final File file;
     public Escritor(String nombreFichero) {
-        File file = new File(nombreFichero);
+        file = new File(nombreFichero);
     }
 
-    public void escribe(int id, int nota) {
-        
+    public void escribe(int id, double nota) throws Exception {
+        FileWriter fw = new FileWriter(file, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(String.format("%d,%.2f\n", id, nota));
+        bw.close();
     }
 
     public void escribe(HashMap<Integer, Double> HashMap) {
