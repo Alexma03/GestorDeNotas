8788e4aff63c294cc41708f5c8fc8d0cdd422ef9
Alex-Laptop <alex03marcos@gmail.com>
2023-03-06 09:56:12 +0100

    Initial commit

---
+++ src/Escritor.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class Escritor {
+    public Escritor(String nombreFichero) {
+        File file = new File(nombreFichero);
+    }
+
+    public void escribe(int id, int nota) {
+        
+    }
+
+    public void escribe(HashMap<Integer, Double> HashMap) {
+
+    }
+}
