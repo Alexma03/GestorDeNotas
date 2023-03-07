cdfa89b5f4516dc95cb962e12b5c4b54b829483d
Alex-Laptop <alex03marcos@gmail.com>
2023-03-07 08:26:13 +0100

    clase escritor metodo lee

--- src/Escritor.java
+++ src/Escritor.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Escritor {
@@ -16,7 +17,12 @@ public class Escritor {
         bw.close();
     }
 
-    public void escribe(HashMap<Integer, Double> HashMap) {
-
+    public void escribe(HashMap<Integer, Double> HashMap) throws IOException {
+        FileWriter fw = new FileWriter(file, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (Integer id : HashMap.keySet()) {
+            bw.write(String.format("%d,%.2f\n", id, HashMap.get(id)));
+        }
+        bw.close();
     }
 }
--- src/Lector.java
+++ src/Lector.java
@@ -1,2 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Lector {
+    private final File file;
+    public Lector(String nombreFichero) {
+        file = new File(nombreFichero);
+    }
+
+    public double lee(int id) throws IOException {
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String linea;
+        while ((linea = br.readLine()) != null) {
+            String[] campos = linea.split(",");
+            if (Integer.parseInt(campos[0]) == id) {
+                br.close();
+                return Double.parseDouble(campos[1]);
+            }
+        }
+        br.close();
+        return -1;
+    }
 }
